a0a92fca2fab74c6b2bc8232dfba4d1100a2611a
PhamNamSon <phamnamson1999@gmail.com>
2025-01-30 10:40:27 -0700

    Implement Bear problem logic with state management, neighbor generation, and cost calculation

--- cmput261_assign1/bear.py
+++ cmput261_assign1/bear.py
@@ -10,31 +10,74 @@ prompt.  DO NOT EDIT the main() function.  You may add additional tests to the
 """
 
 from heapq import heappop, heappush
+from itertools import combinations
 
 
 class Bear_problem(object):
     def __init__(self):
-        pass
+        self.researchers = {
+            "Undergrad": 1.5,
+            "Grad Student": 3,
+            "Postdoc": 7.5,
+            "Professor": 10
+        }
+
+        self.start_state = (
+            frozenset(["Undergrad", "Grad Student", "Postdoc", "Professor"]),
+            frozenset(),
+            "Left"
+        )
+
+        self.end_state = (
+            frozenset(),
+            frozenset(["Undergrad", "Grad Student", "Postdoc", "Professor"]), 
+            "Right"
+        )
 
     def start_node(self):
         """returns start node"""
-        # TODO
-        return (None,)
+        return self.start_state
 
     def is_goal(self, node):
         """is True if `node` is a goal"""
-        # TODO
-        return False
+        return node == self.end_state
 
     def neighbors(self, node):
         """returns a list of the arcs for the neighbors of `node`"""
-        # TODO
-        return []
+        left_side, right_side, light_position = node
+        neighbors = []
+
+        if light_position == "Left":
+            current_side, other_side, next_light = left_side, right_side, "Right"
+        else:
+            current_side, other_side, next_light = right_side, left_side, "Left"
+
+        for moving_group in [{r} for r in current_side] + [set(p) for p in combinations(current_side, 2)]:
+            new_current = current_side - moving_group
+            new_other = other_side | moving_group
+            next_state = (frozenset(new_current), frozenset(new_other), next_light)
+            neighbors.append((node, next_state))
+
+        return neighbors
 
     def arc_cost(self, arc):
         """Returns the cost of `arc`"""
-        # TODO
-        return 0
+        current_state, next_state = arc
+        if current_state is None:
+            return 0
+
+        left_X, right_X, light_X = current_state
+        left_Y, right_Y, light_Y = next_state
+
+        # Determine which side the movement happened on
+        if light_X == "Left" and light_Y == "Right":
+            moved_researchers = right_Y - right_X
+        elif light_X == "Right" and light_Y == "Left":
+            moved_researchers = left_Y - left_X
+        else:
+            return 0  # No valid movement
+
+        return max(self.researchers[person] for person in moved_researchers) if moved_researchers else 0
 
     def cost(self, path):
         """Returns the cost of `path`"""
@@ -42,12 +85,44 @@ class Bear_problem(object):
 
     def heuristic(self, node):
         """Returns the heuristic value of `node`"""
-        # TODO
-        return 0
+        left_side, right_side, light = node
+    
+        if not left_side:
+            return 0  # No one left to move
 
-    def search():
+        if len(left_side) == 1:
+            return self.researchers[next(iter(left_side))]  # Only one person left, return their cost
+
+        # Extract costs for all researchers on the left side
+        costs = sorted([self.researchers[person] for person in left_side], reverse=True)
+
+        # Sum of the two highest costs
+        return sum(costs[:2]) if len(costs) > 1 else costs[0]
+
+    def search(self):
         """Return a solution path"""
-        # TODO
+        frontier = Frontier()
+        start_node = self.start_node()
+        frontier.add([(None, start_node)], 0)  
+        explored = set()  # Use a set for faster lookups
+
+        while not frontier.is_empty():
+            path = frontier.remove()
+            _, current_node = path[-1]  
+
+            if self.is_goal(current_node):
+                return path[1:]  # Remove (None, start_node) from result
+
+            if current_node in explored:
+                continue
+
+            explored.add(current_node)
+
+            for arc in self.neighbors(current_node):
+                new_path = path + [arc]
+                total_cost = self.cost(new_path) + self.heuristic(arc[1])
+                frontier.add(new_path, total_cost)
+
         return None
 
 
